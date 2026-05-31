8572dcd6f7fff48fc7788b416b65e267fc626ee0
Mohamed Nagy <166973454+mohamed-nagy11@users.noreply.github.com>
2026-05-31 23:13:28 +0300

    chore: add vocab_dict.json path to config
    Add the path of the vocab_dict.json file that is used by the emotion classifier.

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 MODELS_DIR = BASE_DIR / "models"
 
+VOCAB_DICT_DIR = MODELS_DIR / "vocab_dict.json"
+
 EMOTION_MODEL_DIR = MODELS_DIR / "emotion_classifier"
 
 LANGUAGE_MODEL_PATH = MODELS_DIR / "language_detector.pkl"
