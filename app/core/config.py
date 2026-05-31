88e8418db47e862a3b670ec1e4fe5f23477e0062
Mohamed Nagy <mohamednagy7777777@gmail.com>
2026-06-01 01:12:03 +0300

    hot-fix: load .env into environment variables

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,14 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv(override=True)
+
+# Directory Paths
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
 MODELS_DIR = BASE_DIR / "models"
-
 VOCAB_DICT_DIR = MODELS_DIR / "vocab_dict.json"
-
 EMOTION_MODEL_DIR = MODELS_DIR / "emotion_classifier.pth"
-
 LANGUAGE_MODEL_PATH = MODELS_DIR / "language_detector.pkl"
 
 # LLM Configuration
