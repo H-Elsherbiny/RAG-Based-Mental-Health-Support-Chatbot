cfa458e91494648bf583a90c8e4193a080463b29
Mohamed1Hatem <mohamedhatemm357@gmail.com>
2026-06-04 15:07:57 +0300

    feat: replaced old language detecion model with hierarchiacl one

--- app/core/config.py
+++ app/core/config.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 MODELS_DIR = BASE_DIR / "models"
 VOCAB_DICT_DIR = MODELS_DIR / "vocab_dict.json"
 EMOTION_MODEL_DIR = MODELS_DIR / "emotion_classifier.pth"
-LANGUAGE_MODEL_DIR = MODELS_DIR / "language_detector.pkl"
+LANGUAGE_MODEL_DIR = MODELS_DIR / "language_detection_models/hierarchical_traditional_pipeline.pkl"
 
 # LLM Configuration
 GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
